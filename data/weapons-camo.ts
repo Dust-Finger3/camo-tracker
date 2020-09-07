1e8bb82ab1a003ad0a92d6476402107d869b61c7
Randy Wenas <randywenas@gmail.com>
2020-09-06 19:30:23 -0700

    Add base data

---
+++ data/weapons-camo.ts
@@ -0,0 +1,306 @@
+import { Weapon, WeaponType } from './data-definitions';
+
+/**
+ * DATA
+ */
+export const ASSAULT: Weapon[] = [
+  {
+    id: 'ak117',
+    name: 'AK-117',
+    type: WeaponType.ASSAULT,
+    camo: {},
+  },
+  {
+    id: 'ak47',
+    name: 'AK-47',
+    type: WeaponType.ASSAULT,
+    camo: {},
+  },
+  {
+    id: 'asm10',
+    name: 'ASM10',
+    type: WeaponType.ASSAULT,
+    camo: {},
+  },
+  {
+    id: 'bk57',
+    name: 'BK57',
+    type: WeaponType.ASSAULT,
+    camo: {},
+  },
+  {
+    id: 'drh',
+    name: 'DR-H',
+    type: WeaponType.ASSAULT,
+    camo: {},
+  },
+  {
+    id: 'hbra3',
+    name: 'HBRa3',
+    type: WeaponType.ASSAULT,
+    camo: {},
+  },
+  {
+    id: 'hvk30',
+    name: 'HVK-30',
+    type: WeaponType.ASSAULT,
+    camo: {},
+  },
+  {
+    id: 'icr1',
+    name: 'ICR-1',
+    type: WeaponType.ASSAULT,
+    camo: {},
+  },
+  {
+    id: 'kn44',
+    name: 'KN-44',
+    type: WeaponType.ASSAULT,
+    camo: {},
+  },
+  {
+    id: 'lk24',
+    name: 'LK24',
+    type: WeaponType.ASSAULT,
+    camo: {},
+  },
+  {
+    id: 'm4',
+    name: 'M4',
+    type: WeaponType.ASSAULT,
+    camo: {},
+  },
+  {
+    id: 'm16',
+    name: 'M16',
+    type: WeaponType.ASSAULT,
+    camo: {},
+  },
+  {
+    id: 'mow',
+    name: 'Man-O-War',
+    type: WeaponType.ASSAULT,
+    camo: {},
+  },
+  {
+    id: 'type25',
+    name: 'Type 25',
+    type: WeaponType.ASSAULT,
+    camo: {},
+  },
+];
+
+export const LMG: Weapon[] = [
+  {
+    id: 'chopper',
+    name: 'Chopper',
+    type: WeaponType.LMG,
+    camo: {},
+  },
+  {
+    id: 'm4lmg',
+    name: 'M4LMG',
+    type: WeaponType.LMG,
+    camo: {},
+  },
+  {
+    id: 'rpd',
+    name: 'RPD',
+    type: WeaponType.LMG,
+    camo: {},
+  },
+  {
+    id: 's36',
+    name: 'S36',
+    type: WeaponType.LMG,
+    camo: {},
+  },
+  {
+    id: 'ul736',
+    name: 'UL736',
+    type: WeaponType.LMG,
+    camo: {},
+  },
+];
+
+export const PISTOL: Weapon[] = [
+  {
+    id: 'j358',
+    name: 'J358',
+    type: WeaponType.PISTOL,
+    camo: {
+      head: 45,
+      long: 25,
+      kills: 200,
+    },
+  },
+  {
+    id: 'mw11',
+    name: 'MW11',
+    type: WeaponType.PISTOL,
+    camo: {
+      head: 45,
+      long: 25,
+      kills: 200,
+    },
+  },
+];
+
+export const SMG: Weapon[] = [
+  {
+    id: 'chicom',
+    name: 'Chicom',
+    type: WeaponType.SMG,
+    camo: {},
+  },
+  {
+    id: 'cordite',
+    name: 'Cordite',
+    type: WeaponType.SMG,
+    camo: {},
+  },
+  {
+    id: 'gks',
+    name: 'GKS',
+    type: WeaponType.SMG,
+    camo: {},
+  },
+  {
+    id: 'hg40',
+    name: 'HG 40',
+    type: WeaponType.SMG,
+    camo: {},
+  },
+  {
+    id: 'msmc',
+    name: 'MSMC',
+    type: WeaponType.SMG,
+    camo: {},
+  },
+  {
+    id: 'pdw57',
+    name: 'PDW-57',
+    type: WeaponType.SMG,
+    camo: {},
+  },
+  {
+    id: 'pharo',
+    name: 'Pharo',
+    type: WeaponType.SMG,
+    camo: {},
+  },
+  {
+    id: 'qq9',
+    name: 'QQ9',
+    type: WeaponType.SMG,
+    camo: {},
+  },
+  {
+    id: 'razor',
+    name: 'Razorback',
+    type: WeaponType.SMG,
+    camo: {},
+  },
+  {
+    id: 'rus79u',
+    name: 'RUS-79U',
+    type: WeaponType.SMG,
+    camo: {},
+  },
+];
+
+export const SNIPER: Weapon[] = [
+  {
+    id: 'arctic50',
+    name: 'Arctic .50',
+    type: WeaponType.SNIPER,
+    camo: {},
+  },
+  {
+    id: 'dlq33',
+    name: 'DL Q33',
+    type: WeaponType.SNIPER,
+    camo: {},
+  },
+  {
+    id: 'locus',
+    name: 'Locus',
+    type: WeaponType.SNIPER,
+    camo: {},
+  },
+  {
+    id: 'm21ebr',
+    name: 'M21 EBR',
+    type: WeaponType.SNIPER,
+    camo: {},
+  },
+  {
+    id: 'outlaw',
+    name: 'Outlaw',
+    type: WeaponType.SNIPER,
+    camo: {},
+  },
+  {
+    id: 'xpr50',
+    name: 'XPR-50',
+    type: WeaponType.SNIPER,
+    camo: {},
+  },
+];
+
+export const SHOTGUN: Weapon[] = [
+  {
+    id: 'by15',
+    name: 'BY15',
+    type: WeaponType.SHOTGUN,
+    camo: {},
+  },
+  {
+    id: 'hs0405',
+    name: 'HS0405',
+    type: WeaponType.SHOTGUN,
+    camo: {},
+  },
+  {
+    id: 'hs2126',
+    name: 'HS2126',
+    type: WeaponType.SHOTGUN,
+    camo: {},
+  },
+  {
+    id: 'krm262',
+    name: 'KRM-262',
+    type: WeaponType.SHOTGUN,
+    camo: {},
+  },
+  {
+    id: 'striker',
+    name: 'Striker',
+    type: WeaponType.SHOTGUN,
+    camo: {},
+  },
+];
+
+export const MARKSMAN: Weapon[] = [
+  {
+    id: 'kilo',
+    name: 'Kilo Bolt-Action',
+    type: WeaponType.MARKSMAN,
+    camo: {},
+  },
+];
+
+export const MELEE: Weapon[] = [
+  {
+    id: 'knife',
+    name: 'KNIFE',
+    type: WeaponType.MELEE,
+    camo: {
+      fromBehind: 30,
+      crouching: 30,
+      deadSilence: 50,
+      kills2NoDie: 30,
+      kills3NoDie: 30,
+    },
+  },
+];
