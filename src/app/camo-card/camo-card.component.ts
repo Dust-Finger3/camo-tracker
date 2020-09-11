c297cfa4ae9553d0befc7c6dd82249251e134520
Dust-Finger3 <dustfinger420@gmail.com>
2020-09-11 09:06:58 +0200

    checked and committed

---
+++ src/app/camo-card/camo-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-camo-card',
+  templateUrl: './camo-card.component.html',
+  styleUrls: ['./camo-card.component.scss']
+})
+export class CamoCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
